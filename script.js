df71b71b2077b3e38c3f948c117b6246cd5397b4
Alek Pialucha <pialuchaa@btinternet.com>
2024-09-27 13:00:03 +0100

    video replace

--- script.js
+++ script.js
@@ -11,6 +11,23 @@ const checkTeamLogos = () => {
   }
 };
 
+const changeBroadcast = () => {
+  console.log("here")
+  const broadcast = document.querySelector("#video-player-placeholder");
+  console.log(broadcast)
+  if (broadcast) {
+    broadcast.src = "https://player.twitch.tv/?channel=caedrel&parent=lolesports.com&autoplay=true&muted=false";
+    broadcast.attributes.muted = false
+    const broadcastLink = document.querySelector("a.pos_absolute.inset_0.z_zLayer3")
+    broadcastLink.href = "https://www.twitch.tv/caedrel";
+    const text = document.querySelector(".pos_absolute.d_flex.flex-d_column.jc_end.ai_start.left_16.bottom_16.z_zLayer2.md\\:left_40.md\\:bottom_40")
+    const gradient = document.querySelector(".pos_absolute.inset_0.d_flex.pointer-events_none.us_none.z_zLayer")
+
+    text.remove()
+    gradient.remove()
+  }
+}
+
 if (document.location.href.indexOf("lolesports") > -1) {
 
   window.addEventListener("load", () => {
@@ -20,6 +37,7 @@ if (document.location.href.indexOf("lolesports") > -1) {
     //MutationObserver to monitor changes in the DOM
     const observer = new MutationObserver(() => {
       checkTeamLogos();
+      changeBroadcast();
     });
 
     //Observer config
